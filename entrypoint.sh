7ed6b67d8280f54f4841f66604b82056092a13d5
Alex Chistyakov <alexclear@gmail.com>
2025-07-13 17:29:31 +0300

    Tweak log redirection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -e
 
-# Start Podman system service in the background
-podman system service -t 0 &
+podman system service -t 0 > /dev/stdout 2>&1 &
+PODMAN_PID=$!
 
-# Wait for the Podman socket to be available
+# Wait for socket
 SOCK="/tmp/podman-run-1001/podman/podman.sock"
 for i in {1..10}; do
     if [ -S "$SOCK" ]; then
         break
     fi
+    echo "Waiting for the Podman socket to appear"
     sleep 1
 done
 
-# Start Forgejo runner daemon, using Podman socket
+# Start Forgejo runner
 export DOCKER_HOST="unix://${SOCK}"
-./forgejo-runner register \
-    --no-interactive \
-    --token "${FORGEJO_TOKEN}" \
-    --instance "${FORGEJO_URL}"
-exec ./forgejo-runner daemon
+./forgejo-runner register --no-interactive --token "${FORGEJO_TOKEN}" --instance "${FORGEJO_URL}"
+
+# Wait for both processes (podman + runner)
+./forgejo-runner daemon > /dev/stdout 2>&1 &
+
+wait -n $PODMAN_PID
